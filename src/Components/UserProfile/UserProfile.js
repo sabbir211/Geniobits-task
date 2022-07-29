2bdd5bb341b8fd24a969a4c41f0b4729ad2edc5f
sabbir <sabbir21122@gmail.com>
2022-07-29 21:47:02 +0600

    scratch

---
+++ src/Components/UserProfile/UserProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserProfile = () => {
+    return (
+        <div>
+            <h1> I am user profile</h1>
+        </div>
+    );
+};
+
+export default UserProfile;
