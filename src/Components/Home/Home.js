93f031f8e17a3f8f6801f25303e74ff50aea0e5a
sabbir <sabbir21122@gmail.com>
2022-07-29 23:06:35 +0600

    side bar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,8 @@
+import { faCommentDots } from '@fortawesome/free-regular-svg-icons';
+import { faGear, faHome, faNotEqual, faPeopleArrows, faPerson, faStopwatch, faSuitcaseRolling, faUserGraduate, faUsersLine } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-
+import "./homeStyle.css"
 const Home = () => {
     return (
         <div class="drawer drawer-mobile ">
@@ -11,10 +14,54 @@ const Home = () => {
             </div>
             <div class="drawer-side shadow-2xl rounded-3xl">
                 <label for="my-drawer-2" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                    {/* Sidebar content here  */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                {/* Sidebar content here  */}
+                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content drawer-links">
+
+                    <div class="form-control mt-4">
+                        <div class="input-group">
+                            <input type="text" placeholder="Search…" class="input border-b-4 border-indigo-500 focus:outline-none w-full " />
+                            <button class="btn btn-square btn-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                            </button>
+                        </div>
+                    </div>
+                    <li className='mt-4'>
+                        
+                        <span><FontAwesomeIcon icon={faUserGraduate}/> Admin</span>
+                    </li>
+                    <li>
+                        {/* <span></span> */}
+                        <span> <FontAwesomeIcon icon={faUsersLine}/> PIM</span>
+                    </li>
+                    <li>
+                        {/* <span></span> */}
+                        <span><FontAwesomeIcon icon={faSuitcaseRolling}/> Leave</span>
+                    </li>
+                    <li>
+                        {/* <span></span> */}
+                        <span> <FontAwesomeIcon icon={faStopwatch}/> Time</span>
+                    </li>
+                    <li>
+                        {/* <span></span> */}
+                        <span> <FontAwesomeIcon icon={faPerson}/> My info</span>
+                    </li>
+                    <li>
+                        {/* <span></span> */}
+                        <span> <FontAwesomeIcon icon={faHome}/> Dashboard</span>
+                    </li>
+                    <li>
+                        {/* <span></span> */}
+                        <span> <FontAwesomeIcon icon={faNotEqual}/> Directory</span>
+                    </li>
+                    <li>
+                        {/* <span></span> */}
+                        <span><FontAwesomeIcon icon={faGear}/> Maintenance</span>
+                    </li>
+                    <li>
+                        {/* <span></span> */}
+                        <span><FontAwesomeIcon icon={faCommentDots}/> Buzz</span>
+                    </li>
+                    {/* <li><a>Sidebar Item 2</a></li> */}
                 </ul>
 
             </div>
--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -2,18 +2,37 @@ import React from 'react';
 
 const NavigationBar = () => {
     return (
-        <div class="navbar bg-gradient-to-r from-purple-500 to-pink-500 text-white shadow-lg">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <div class="bg-gradient-to-r from-purple-500 to-pink-500 text-white shadow-lg">
+            <div class="navbar  container mx-auto">
+                <div class="navbar-start">
+                    <div class="dropdown">
+                        <label tabindex="0" class="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>Item 1</a></li>
+                            <li tabindex="0">
+                                <a class="justify-between">
+                                    Parent
+                                    <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+                                </a>
+                                <ul class="p-2">
+                                    <li><a>Submenu 1</a></li>
+                                    <li><a>Submenu 2</a></li>
+                                </ul>
+                            </li>
+                            <li><a>Item 3</a></li>
+                        </ul>
+                    </div>
+                    <a class="btn btn-ghost normal-case text-xl">Geniobits</a>
+                </div>
+                <div class="navbar-center hidden lg:flex">
+                    <ul class="menu menu-horizontal p-0">
                         <li><a>Item 1</a></li>
                         <li tabindex="0">
-                            <a class="justify-between">
+                            <a>
                                 Parent
-                                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+                                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                             </a>
                             <ul class="p-2">
                                 <li><a>Submenu 1</a></li>
@@ -23,28 +42,22 @@ const NavigationBar = () => {
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-            </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal p-0">
-                    <li><a>Item 1</a></li>
-                    <li tabindex="0">
-                        <a>
-                            Parent
-                            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
-                        <ul class="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
-            </div>
-            <div class="navbar-end">
-                <a class="btn">Get started</a>
+                <div class="navbar-end ">
+                    <div className='flex rounded-full items-center pr-2  bg-pink-400  lg:mr-8 lg:pr-4'>
+                        <div class="avatar">
+                            <div class="w-12 rounded-full">
+                                <img src="https://placeimg.com/192/192/people" />
+                            </div>
+                        </div>
+                        <div className='text-xl font-semibold ml-2'>
+                            <p>User name</p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
+
     );
 };
 
